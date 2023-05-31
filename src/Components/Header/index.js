16024f02b03d885e5224c4a46915c8e9443a0c60
bdalberson <Brian.alberson@gmail.com>
2023-05-30 17:00:14 -0700

    files

---
+++ src/Components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+
+    return(
+        <div>
+            <h2>Hello world</h2>
+            <h2>Now with outter div</h2>
+        </div> 
+    )
+}
+
+export default Header;
